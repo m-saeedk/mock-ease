4680e218676b87e3c3e7848ef8cb8d4dc956873c
Msaeed1999 <muhammad.saeed918@gmail.com>
2024-05-25 16:59:21 +0500

    updated

---
+++ lib/mock-ease.d.ts
@@ -0,0 +1,91 @@
+declare class MockServer {
+    private serverName;
+    private server;
+    private router;
+    private _routePrefix;
+    constructor(serverName?: string);
+    private joinPath;
+    EnableAuth(dummyToken: string, headerKey?: string): this;
+    /**
+     * Sets the route prefix for the mock server.
+     *
+     * @param prefix - The route prefix to be set.
+     * @returns The current instance of the `MockServer` class.
+     */
+    SetRoutePrefix(prefix: string): this;
+    /**
+     * Binds CRUD routes for a given module.
+     * @param moduleName - The name of the module.
+     * @param schema - A function that returns the schema for the module.
+     * @param maxResults - The maximum number of results to return (optional).
+     * @returns The current instance of the `MockServer` class.
+     */
+    BindCrudRoutes(moduleName: string, schema: () => {
+        [key: string]: any;
+    }, maxResults?: number): this;
+    /**
+     * Binds a new GET route to the server.
+     *
+     * @param route - The route path for the GET request.
+     * @param responseSchema - A function that returns the response schema for the route.
+     * @param maxResults - The maximum number of results to be returned (optional).
+     * @returns The instance of the server.
+     */
+    BindNewGETRoute(route: string, responseSchema: () => {
+        [key: string]: any;
+    }, maxResults?: number): this;
+    /**
+     * Binds a new POST route to the server.
+     *
+     * @param route - The route path for the POST request.
+     * @param responseSchema - A function that returns the response for the route.
+     * @returns - The current instance of the MockServer.
+     */
+    BindNewPOSTRoute(route: string, responseSchema: () => {
+        [key: string]: any;
+    }): MockServer;
+    /**
+     * Binds a new PUT route to the server.
+     *
+     * @param route - The route to bind.
+     * @param responseSchema - A function that returns the response schema.
+     * @returns - The current instance of the MockServer.
+     */
+    BindNewPUTRoute(route: string, responseSchema: () => {
+        [key: string]: any;
+    }): this;
+    /**
+     * Binds a new DELETE route to the server.
+     *
+     * @param route - The route path for the DELETE request.
+     * @param responseSchema - A function that returns the response schema for the DELETE request.
+     * @returns - The current instance of the MockServer.
+     */
+    BindNewDELETERoute(route: string, responseSchema: () => {
+        [key: string]: any;
+    }): this;
+    /**
+     * Binds a new PATCH route to the server.
+     *
+     * @param route - The route path for the PATCH request.
+     * @param responseSchema - A function that returns the response schema for the route.
+     * @returns - The current instance of the MockServer.
+     */
+    BindNewPATCHRoute(route: string, responseSchema: () => {
+        [key: string]: any;
+    }): this;
+    /**
+     * Starts the mock server on the specified port.
+     * @param port - The port number to listen on. Defaults to 3005 if not provided.
+     */
+    Start(port?: number): void;
+    logRoutes(): void;
+}
+/**
+ * Creates a new instance of the MockServer class.
+ *
+ * @param serverName - Optional name for the server.
+ * @returns A new instance of the MockServer class.
+ */
+export declare function CreateNewServer(serverName?: string): MockServer;
+export {};
